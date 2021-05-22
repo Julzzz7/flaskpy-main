1612dfb214cd261d957f654ff9bacdc1ae57d460
Julzzz7 <80964222+Julzzz7@users.noreply.github.com>
2021-05-22 13:59:20 +0700

    Update clientimage.py

--- clientimage.py
+++ clientimage.py
@@ -1,6 +1,6 @@
 import requests
 
-resp = requests.post("http://localhost:5000/predict",
+resp = requests.post("https://implementasijulianak.herokuapp.com/predict",
                      files={"file": open('C://olahcitra//kodok1.jpg','rb')})
 
 print(resp.json())
